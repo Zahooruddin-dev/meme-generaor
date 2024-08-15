2b2ca1ef227a757d188c9c5e492102aba8fdc018
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-15 14:49:57 +0500

    added meme image
    images are called from the database using what i had learned with State

--- meme/src/components/Meme.jsx
+++ meme/src/components/Meme.jsx
@@ -1,43 +1,48 @@
 /**
- * Challenge:
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
+ * Note: if you ever need the old value of state
+ * to help you determine the new value of state,
+ * you should pass a callback function to your
+ * state setter function instead of using
+ * state directly. This callback function will
+ * receive the old value of state as its parameter,
+ * which you can then use to determine your new
+ * value of state.
  */
-import memeData from "../memeData";
- /**
-     * Challenge: Get a random image from the `memesData` array
-     * when the "new meme image" button is clicked.
-     * 
-     * Log the URL of the image to the console. (Don't worry
-     * about displaying the image yet)
+   /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
      */
-    function memed(){
-      const memeImg = memeData.data.memes
-      const randomNumber = Math.floor(Math.random() * memeImg.length)
-      const url =memeImg[randomNumber].url
-      console.log(url);
-      
- 
-    }
+import memeData from '../memeData';
+// Must import react from import otherwise the State won't work
+import React from "react"
+
+
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("")
+
+  function memed() {
+    const memeImg = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeImg.length);
+    const url = memeImg[randomNumber].url;
+    console.log(url);
+    setMemeImage(url)
+  }
 	return (
 		<main>
-			<div className="form">
-				<input 
-        type='text' 
-        className='form-input'
-        placeholder="Top-text" />
-				<input 
-        type='text' 
-        className='form-input' 
-        placeholder="Bottom-text"/>
-				<button 
-        type='button'
-        className="form--button"
-        onClick={memed}>Get a new meme image</button>
+			<div className='form'>
+				<input type='text' className='form-input' placeholder='Top-text' />
+				<input type='text' className='form-input' placeholder='Bottom-text' />
+				<button type='button' className='form--button' onClick={memed}>
+					Get a new meme image
+				</button>
 			</div>
+      <img src={memeImage} className='meme-img'/>
 		</main>
 	);
 }
