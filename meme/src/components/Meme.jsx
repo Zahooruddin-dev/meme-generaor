7ca0a1b45fb034affbaae703fc30c351e11acb3f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-22 15:48:11 +0500

    added top and bottom text editor

--- meme/src/components/Meme.jsx
+++ meme/src/components/Meme.jsx
@@ -1,38 +1,21 @@
 /**
- * Note: if you ever need the old value of state
- * to help you determine the new value of state,
- * you should pass a callback function to your
- * state setter function instead of using
- * state directly. This callback function will
- * receive the old value of state as its parameter,
- * which you can then use to determine your new
- * value of state.
- */
-   /**
-     * Challenge: Save the random meme URL in state
-     * - Create new state called `memeImage` with an
-     *   empty string as default
-     * - When the getMemeImage function is called, update
-     *   the `memeImage` state to be the random chosen
-     *   image URL
-     * - Below the div.form, add an <img /> and set the
-     *   src to the new `memeImage` state you created
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+    })
      */
-import memeData from '../memeData';
-// Must import react from import otherwise the State won't work
-import React from "react"
-
-
-export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("")
-
-  function memed() {
-    const memeImg = memeData.data.memes;
-    const randomNumber = Math.floor(Math.random() * memeImg.length);
-    const url = memeImg[randomNumber].url;
-    console.log(url);
-    setMemeImage(url)
-  }
+		/*
 	return (
 		<main>
 			<div className='form'>
@@ -42,7 +25,80 @@ export default function Meme() {
 					Get a new meme image
 				</button>
 			</div>
-      <img src={memeImage} className='meme-img'/>
+			<img src={meme.randomImage} className='meme-img' />
+			
 		</main>
 	);
+	  function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+}*/
+
+import memeData from '../memeData';
+// Must import react from import otherwise the State won't work
+import React from 'react';
+
+export default function Meme() {
+	const [meme, setMeme] = React.useState({
+		topText: '',
+		bottomText: '',
+		randomImage: 'http://i.imgflip.com/1bij.jpg',
+	});
+	const [allMemeImages, setAllMemesImages] = React.useState(memeData);
+	function memed() {
+		const memesArray = allMemeImages.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[randomNumber].url;
+		setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }));
+	}
+
+  function handleChange(event){
+	const{name, value} = event.target		
+	setMeme(prevMeme => ({
+		...prevMeme,
+		[name]:value 
+	}));
+	}
+return (
+	<main>
+			<div className="form">
+					<input 
+							type="text"
+							placeholder="Top text"
+							className="form--input"
+							name='topText'
+							value={meme.topText}                    
+							onChange={handleChange}
+
+
+					/>
+					<input 
+							type="text"
+							placeholder="Bottom text"
+							className="form--input"
+							name='bottomText'
+							value={meme.bottomText}
+							onChange={handleChange}
+					/>
+					<button 
+							className="form--button"
+							onClick={memed}
+					>
+							Get a new meme image 🖼
+					</button>
+			</div>
+			<div className="meme">
+					<img src={meme.randomImage} className="meme--image" />
+					<h2 className="meme--text top">{meme.topText}</h2>
+					<h2 className="meme--text bottom">{meme.bottomText}</h2>
+			</div>
+	</main>
+)
 }
